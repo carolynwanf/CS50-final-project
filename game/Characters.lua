abea37512baafebb8894819d8b59acaebbe4fe03
janxoverride <arielwang20@gmail.com>
2019-12-03 22:04:42 -0500

    fixed conflict v2

--- game/Characters.lua
+++ game/Characters.lua
@@ -33,11 +33,6 @@ function Characters:init(map)
     self.spyx =map.tileWidth * 30 * 2
 end
 
-<<<<<<< HEAD
-function Characters:render()
-    love.graphics.draw(self.spritesheet, self.spySprite, self.spyx, self.y)
-=======
->>>>>>> 66d83bfb2cf1e132043b2d4f3f96d43a486b6c57
 function Characters:inRange(tile)
     -- define NPC tiles
     local collidables = {
@@ -54,12 +49,8 @@ function Characters:inRange(tile)
     return false
 end
 
-<<<<<<< HEAD
-        
-=======
 function Characters:render()
     love.graphics.draw(self.spritesheet, self.spySprite, self.spyx, self.y)
->>>>>>> 66d83bfb2cf1e132043b2d4f3f96d43a486b6c57
     love.graphics.draw(self.spritesheet, self.n1Sprite, self.n1x, self.y)
     love.graphics.draw(self.spritesheet, self.n2Sprite, self.n2x, self.y)
     love.graphics.draw(self.spritesheet, self.n3Sprite, self.n3x, self.y)
